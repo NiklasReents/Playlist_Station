23fb1ff80d8e3989c1aaf32fb65f2bd6562486c4
Niklas Reents <Niklas.Reents@gmx.net>
2022-01-30 23:09:19 +0100

    add fetchimage function to fetchhandler

--- src/modules/fetchHandler.js
+++ src/modules/fetchHandler.js
@@ -1,10 +1,20 @@
 class FetchHandler {
-  constructor(axios, playlistUrl, cookie, controller, setPlaylistData) {
+  constructor(
+    axios,
+    playlistUrl,
+    cookie,
+    controller,
+    setPlaylistData,
+    imgUrl,
+    setImgFile
+  ) {
     this.axios = axios;
     this.playlistUrl = playlistUrl;
     this.cookie = cookie;
     this.controller = controller;
     this.setPlaylistData = setPlaylistData;
+    this.imgUrl = imgUrl;
+    this.setImgFile = setImgFile;
   }
 
   async fetchData(url, method, decode) {
@@ -26,5 +36,14 @@ class FetchHandler {
       .catch((err) => console.error(err));
   };
 }
+
+fetchImage = () => {
+  this.fetchData(this.imgUrl, "get", "blob")
+    .then((img) => {
+      const fileUrl = URL.createObjectURL(img);
+      this.setImgFile(fileUrl);
+    })
+    .catch((err) => console.error(err));
+};
 
 export { FetchHandler };
