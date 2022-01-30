44f4b67c541dea02dee53b7b0a3415deaadf2c65
Niklas Reents <Niklas.Reents@gmx.net>
2022-01-30 18:27:00 +0100

    create songimage component

---
+++ src/components/SongImage.js
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+
+export default function SongImage(props) {
+  const { currentImg, imgSrc } = props;
+  const imgElement = useRef();
+  const progressBar = useRef();
+
+  return (
+    <td id="image-element" style={{ display: currentImg }}>
+      <div>
+        <img src="images/minus.png" alt="Decrease Volume" />
+        <img ref={imgElement} src={imgSrc} alt="" />
+        <img src="images/plus.png" alt="Increase Volume" />
+      </div>
+      <div id="progress-bar-container">
+        <div ref={progressBar} id="progress-bar"></div>
+      </div>
+    </td>
+  );
+}
