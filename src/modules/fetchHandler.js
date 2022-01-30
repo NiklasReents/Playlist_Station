3db5597379912c47e8e5fb1982d18bd90e5863fa
Niklas Reents <Niklas.Reents@gmx.net>
2022-01-30 19:01:53 +0100

    create fetchhandler module for app fetching processes

---
+++ src/modules/fetchHandler.js
@@ -0,0 +1,30 @@
+class FetchHandler {
+  constructor(axios, playlistUrl, cookie, controller, setPlaylistData) {
+    this.axios = axios;
+    this.playlistUrl = playlistUrl;
+    this.cookie = cookie;
+    this.controller = controller;
+    this.setPlaylistData = setPlaylistData;
+  }
+
+  async fetchData(url, method, decode) {
+    const response = await this.axios({
+      url: url,
+      method: method,
+      headers: url === this.playlistUrl && {
+        authorization: this.cookie.get("Jwt"),
+      },
+      responseType: decode,
+      signal: this.controller.signal,
+    });
+    return response.data;
+  }
+
+  fetchList = () => {
+    this.fetchData(this.playlistUrl, "get", "json")
+      .then((json) => this.setPlaylistData(json))
+      .catch((err) => console.error(err));
+  };
+}
+
+export { FetchHandler };
